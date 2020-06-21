6cd3dbce92bd4e90eab4bbfb50c2f38ced928306
liuwenda <liuwenda1992>
2020-06-21 16:13:33 +0800

    add http function

--- curlTool.go
+++ curlTool.go
@@ -1,6 +1,8 @@
 package tools
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/astaxie/beego/context"
 	"github.com/astaxie/beego/httplib"
 	"github.com/astaxie/beego/logs"
@@ -78,4 +80,51 @@ func HttpBeegoGet(url string, header map[string]string) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+
+func HttpRequest2Map(ctx *context.Context) (map[string]interface{}, error) {
+	if ctx.Request.Method == "GET" {
+		return httpForm2Map(ctx)
+
+	} else if ctx.Request.Method == "POST" {
+		return httpBody2Map(ctx)
+
+	}
+
+	return nil, errors.New("unSupport http method")
+}
+
+func httpBody2Map(ctx *context.Context) (map[string]interface{}, error) {
+	r := ctx.Request
+	m := make(map[string]interface{})
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func httpForm2Map(ctx *context.Context) (data map[string]interface{}, err error) {
+	r := ctx.Request
+	data = make(map[string]interface{})
+	err = r.ParseForm()
+	if err != nil {
+		return
+	}
+
+	if len(r.Form) == 0 {
+		return nil, errors.New("输入参数为空")
+	}
+	for i, v := range r.Form {
+		if len(v) == 0 || len(v) > 1 {
+			return nil, errors.New("表单输入不合法")
+		}
+		data[i] = v[0]
+	}
+	return
 }
